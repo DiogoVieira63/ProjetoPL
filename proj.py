29e5f375cd9190d779fb7f187eda5840f943fb6b
diogovieira63 <a93251@alunos.uminho.pt>
2022-03-17 00:12:26 +0000

    update

--- proj.py
+++ proj.py
@@ -1,26 +1,90 @@
 
+import re
 import ply.lex as lex
 
-tokens = ["SEP","TEXT"]
+import json
+import statistics
 
 
 
+tokens = ["SKIP","SEP","TEXT","NEWLINE","MULT","FUNC"]
 
-def t_SEP(t):
+
+states = [
+    ("header","exclusive")
+]
+
+def t_header_SKIP(t):
+    r'(,{2,}\n*|,\n)'
+    if '\n' in t.lexer.lexmatch[0]:
+        t_header_NEWLINE(t)
+
+
+
+def t_SKIP(t):
+    r'(,{2,}\n*|,\n)'
+    match = t.lexer.lexmatch[0]
+    count = (match.count(',')) + ('\n' in match) -1
+    for i in range(count):
+        t.lexer.line.append(None)
+    if '\n' in match:
+        t_NEWLINE(t)
+
+
+def t_header_FUNC(t):
+    r'::(?P<func>\w+)'
+    func = (t.lexer.lexmatch.group('func'))
+    last = t.lexer.header[-1]
+    if last[2] is not []:
+        last[2].append(func)
+    else:
+        t.lexer.header[-1]=(last[0],last[1],[func])
+
+
+def t_ANY_SEP(t):
     r','
 
-def t_TEXT(t):
+
+def t_header_MULT(t):
+    r'{(?P<number>\d+)(,(?P<number2>\d+))?}'
+    match = t.lexer.lexmatch
+    if match.group('number2') is not None:
+        value= int(match.group('number2'))
+    else:
+        value= int(match.group('number'))
+
+    elem = lexer.header[-1]
+    lexer.header[-1]=(elem[0],value,[])
+
+def t_header_TEXT(t):
     r'\w+'
-    print(t.value)
+    t.lexer.header.append((t.value,1,[]))
+
+def t_TEXT(t):
+    r'(?![ .]+)[\w ]+'
+    t.lexer.line.append(t.value)
+
+def t_header_NEWLINE(t):
+    r'\n'
+    t.lexer.begin("INITIAL")
+
+def t_NEWLINE(t):
+    r'\n'
+    t.lexer.values.append(t.lexer.line)
+    t.lexer.line=[]
 
 
-def t_error(t):
+def t_ANY_error(t):
     print("Error")
 
 lexer = lex.lex()
-lexer.comment= False
+lexer.header=[]
+lexer.values=[]
+lexer.line=[]
 
-f = open("texto.txt","r")
+lexer.begin("header")
+
+f = open("alunos.csv","r")
 
 texto = f.read()
 
@@ -29,6 +93,63 @@ lexer.input(texto)
 
 for tok in lexer:
     print(tok)
+
+
+print(lexer.header)
+print(lexer.values)
+
+
+
+
+
+def doFunc(func,list):
+    list = [int(i) for i in list]
+    if func == "sum":
+        return sum(list)
+    elif func == "media":
+        return (list)/len(list)
+    elif func == "median":
+        return statistics.median(list)
+    elif func== "mode":
+        return statistics.mode(list)
+    elif func== "range":
+        return max(list)-min(list)  
+
+
+
+listDict=[]
+for listLine in lexer.values:
+    dictLine = dict()
+    indexLine=0
+    for (header,hNum,other) in lexer.header:
+        if (hNum == 1):
+            dictLine[header]=listLine[indexLine]
+            indexLine+=1
+        else: 
+            list=[]
+            for i in range(hNum):
+                if listLine[indexLine+i] is not None :
+                    list.append(listLine[indexLine+i])
+            indexLine+=hNum
+            if other != []:
+                for func in other:
+                    dictLine[header+"_"+func]=doFunc(func,list)
+            else:
+                dictLine[header]=list
+                    
+    
+    listDict.append(dictLine)
+
+
+
+# convert into JSON:
+y = json.dumps(listDict, indent=4,ensure_ascii=False)
+
+f.close()
+
+f = open("testeJSON.json","w")
+
+f.write(y)
 
 
 f.close()
