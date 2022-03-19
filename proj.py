8023171504fa9e022d4d11dbe2d713c780374c97
diogovieira63 <a93251@alunos.uminho.pt>
2022-03-19 21:49:14 +0000

    add func to writeJSON

--- proj.py
+++ proj.py
@@ -1,5 +1,7 @@
 
 import re
+from textwrap import indent
+from typing import final
 import ply.lex as lex
 
 import json
@@ -25,8 +27,8 @@ def t_SKIP(t):
     r'(,{2,}\n*|,\n)'
     match = t.value
     count = (match.count(',')) + ('\n' in match) -1
-    #for i in range(count):
-    t.lexer.line.append(None)
+    for i in range(count):
+        t.lexer.line.append(None)
     if '\n' in match:
         t_NEWLINE(t)
 
@@ -57,11 +59,12 @@ def t_header_MULT(t):
     lexer.header[-1]=(elem[0],value,[])
 
 def t_header_TEXT(t):
-    r'(?![ \t]+$)[\w ]+'
+    r'(?![ \t]+$)(["\'].+["\']|[^{},\n]+)'
+    t.value = re.sub(r'["\']',"",t.value)
     t.lexer.header.append((t.value,1,[]))
 
 def t_TEXT(t):
-    r'(?![ \t]+$)[\w ]+'
+    r'(?![ \t]+$)(".+"|[^,\n]+)'
     t.lexer.line.append(t.value)
 
 def t_header_NEWLINE(t):
@@ -84,7 +87,7 @@ lexer.line=[]
 
 lexer.begin("header")
 
-file = "testes/alunos3"
+file = "testes/emd"
 
 f = open(file +".csv","r")
 
@@ -98,11 +101,11 @@ for tok in lexer:
 
 
 print(lexer.header)
-print(lexer.values)
+#print(lexer.values)
 
 
 def doFunc(func,list):
-    list = [int(i) for i in list]
+    list = [float(i) for i in list]
     if func == "sum":
         return sum(list)
     elif func == "media":
@@ -122,7 +125,8 @@ for listLine in lexer.values:
     indexLine=0
     for (header,hNum,other) in lexer.header:
         if (hNum == 1):
-            dictLine[header]=listLine[indexLine]
+            if listLine[indexLine] is not None:
+                dictLine[header]=listLine[indexLine]
             indexLine+=1
         else: 
             list=[]
@@ -139,8 +143,45 @@ for listLine in lexer.values:
                 dictLine[header]=list
     listDict.append(dictLine)
 
+indent =4
+
+def spaces(n):
+    return " " *(indent *n)
+
+def writeDict(listDict):
+    finalStr= "[\n"
+    for indexDict,dict in enumerate(listDict):
+        finalStr+=spaces(1)
+        finalStr+="{\n"
+        for index,(key,elem) in enumerate(dict.items()):
+            finalStr+=spaces(2)
+            if type(elem) is str:
+                finalStr+=f'"{key}": "{elem}"'
+            elif type(elem) is int or type(elem) is float:
+                finalStr+=f'"{key}": {elem}'
+            else:
+                finalStr+='"' + key + '": '
+                finalStr+="["
+                finalStr+=",".join(map(str,list))
+                finalStr+="]\n" 
+
+            if index != len(dict)-1:
+                finalStr+=","
+            finalStr+="\n"  
+        finalStr+=spaces(1)
+        finalStr+="}"
+        if indexDict != len(listDict)-1:
+            finalStr+=","
+        finalStr+="\n"
+    finalStr+="]\n"
+    return finalStr
+
+print(listDict)
+
+y = writeDict(listDict)
+
 # convert into JSON:
-y = json.dumps(listDict, indent=4,ensure_ascii=False)
+#y = json.dumps(listDict, indent=4,ensure_ascii=False)
 
 f.close()
 
@@ -149,3 +190,5 @@ f = open(file + ".json","w")
 f.write(y)
 
 f.close()
+
+
